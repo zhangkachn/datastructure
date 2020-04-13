210a5c59a2efa99dc7ac40cd6ae9a79d7ac64f54
zhangkang56 <zhangkachn@gmail.com>
2020-04-13 08:58:01 +0800

    时间复杂度

---
+++ 复杂度/src/main/java/com/zhangkachn/on/时间复杂度Application.java
@@ -0,0 +1,13 @@
+package com.zhangkachn.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 时间复杂度Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(时间复杂度Application.class, args);
+    }
+
+}
---
+++ 复杂度/src/test/java/com/zhangkachn/on/时间复杂度ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangkachn.on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 时间复杂度ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
